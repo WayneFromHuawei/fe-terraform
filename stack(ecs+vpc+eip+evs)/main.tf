a84139df7e578c2601fd7106c3a0d19038347be6
WayneFromHuawei <wayne.wan@huawei.com>
2017-11-01 15:26:02 +0100

    stack (ecs+vpc+eip+evs)

---
+++ stack(ecs+vpc+eip+evs)/main.tf
@@ -0,0 +1,34 @@
+#a statck to create vpc/ecs/security group and rules using pre-defined modules
+module "vpc" {
+    source = "..//modules//vpc"
+    name   = "${var.project}-net"
+    subnet = "${var.subnet_cidr}"
+	nameserver = "${var.nameserver}"
+}
+
+module "ecs" {
+    source     = "..//modules//ecs"
+	region	   = "${var.region}"
+	az	   	   = "${var.az}"
+    ecs_count  = "${var.instance_count}"
+    name       = "${var.project}-server"
+    image_id   = "${var.image_id}"
+    flavor     = "${var.flavor}"
+    keypair    = "${var.keypair}"	
+    network_id = "${module.vpc.network_id}"
+    subnet_id  = "${module.vpc.subnet_id}"
+	security_groups = ["${var.security_groups}"]
+	user_data  = "${var.user_data}"
+	metadata_this   = "${var.metadata}"
+	diff_hosts_id = ["${var.diff_hosts_id}"]
+}
+
+module "evs" {
+	source = "..//modules//evs"
+	region = "${var.region}"
+	az	   = "${var.az}"
+	name = "${var.project}-volume"
+	size_in_gb = "${var.disk_size_in_gb}"
+	ecs_ids = ["${module.ecs.ids}"]
+	evs_count = "${var.instance_count}"
+}
---
+++ stack(ecs+vpc+eip+evs)/vars_example.tfvars
@@ -0,0 +1,13 @@
+region="eu-west-0"
+az="eu-west-0a"
+project="terrtest-stack"
+image_id="ab30dd89-ca93-4e0d-b23d-75fecdea8e4e"
+flavor="t2.micro"
+keypair="KeyPair-wanwei-ocb"
+metadata_this="that"
+diff_hosts_id=["a43fbeeb-1bcf-4b38-b7b0-da69ae850cd8"] 
+user_data=""
+attach_eip="false"
+instance_count="1"
+subnet_cidr="192.168.112.0/24"
+security_groups=["1d52c37c-e27f-422a-b508-f00f2f25fe0a"]

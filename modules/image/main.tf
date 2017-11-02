62bac5614024f01db1d9017aadb2e3be65358a09
WayneFromHuawei <wayne.wan@huawei.com>
2017-11-02 14:45:52 +0100

    fix bug of image module

--- modules/image/main.tf
+++ modules/image/main.tf
@@ -1,6 +1,6 @@
 resource "openstack_images_image_v2" "image" {
-	region				= "{$var.region}"
-	name   				= "{$var.name}"
+	region				= "${var.region}"
+	name   				= "${var.name}"
 	#local_file_path 	= "${var.local_file_path}"
 	image_source_url 	= "${var.image_source_url}"
 	container_format	= "${var.container_format}"

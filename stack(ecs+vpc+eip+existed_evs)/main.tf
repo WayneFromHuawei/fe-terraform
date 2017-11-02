91b7a8456dcdbd9e7a557f872f7f1d3176000944
WayneFromHuawei <wayne.wan@huawei.com>
2017-11-02 11:26:53 +0100

    stack with existed evs

---
+++ stack(ecs+vpc+eip+existed_evs)/main.tf
@@ -0,0 +1,29 @@
+module "vpc" {
+    source = "..//modules//vpc"
+    name   = "${var.project}-net"
+    subnet = "${var.subnet_cidr}"
+	nameserver = "${var.nameserver}"
+}
+
+module "ecs" {
+    source     = "..//modules//ecs"
+	region	   = "${var.region}"
+	az	   	   = "${var.az}"
+    ecs_count  = "${var.instance_count}"
+    name       = "${var.project}-server"
+    image_id   = "${var.image_id}"
+    flavor     = "${var.flavor}"
+    keypair    = "${var.keypair}"	
+    network_id = "${module.vpc.network_id}"
+    subnet_id  = "${module.vpc.subnet_id}"
+	security_groups = ["${var.security_groups}"]
+	user_data  = "${var.user_data}"
+	metadata_this   = "${var.metadata}"
+	diff_hosts_id = ["${var.diff_hosts_id}"]
+}
+
+resource "openstack_compute_volume_attach_v2" "volume_attach" {
+	count = "${var.instance_count}"
+	instance_id = "${element(module.ecs.ids, count.index)}"
+	volume_id   = "${var.volume_id}"
+}
---
+++ stack(ecs+vpc+eip+existed_evs)/outputs.tf
@@ -0,0 +1,24 @@
+output "addresses" {
+    description = "list of ipv4 addresses of the created servers"
+    value       = ["${module.ecs.addresses}"]
+}
+
+output "ecs_ids" {
+    description = "list of IDs of the created servers"
+    value       = ["${module.ecs.ids}"]
+}
+
+output "count" {
+    description = "number of servers created"
+    value       = "${module.ecs.count}"
+}
+
+output "network_id" {
+    description = "the id of the created network"
+    value       = "${module.vpc.network_id}"
+}
+
+output "subnet_id" {
+    description = "the id of the created subnet"
+    value       = "${module.vpc.subnet_id}"
+}

1b2c7b70533d3e86219236fc2ed535ee395b7aab
WayneFromHuawei <wayne.wan@huawei.com>
2017-11-21 18:52:57 +0100

    bug fix of stack vip

--- stack(vip+snat)/main.tf
+++ stack(vip+snat)/main.tf
@@ -1,42 +1,51 @@
-module "ecs-master" {
-  source          = "..//modules//ecs"
-  region          = "${var.region}"
-  az              = "${var.az}"
-  ecs_count       = "${var.instance_count}"
-  name            = "${var.project}-server-master"
-  image_id        = "${var.image_id}"
-  flavor          = "${var.flavor}"
-  keypair         = "${var.keypair}"
-  network_id      = "${var.network_id}"
-  subnet_id       = "${var.subnet_id}"
-  security_groups = ["${var.security_groups}"]
-
-  allowed_address_pairs = [{"ip_address"="${openstack_networking_port_v2.port_vip.all_fixed_ips[0]}"}]
-  }]
-}
-
-module "ecs-standby" {
-  source     = "..//modules//ecs"
-  region     = "${var.region}"
-  az         = "${var.az}"
-  ecs_count  = "${var.instance_count}"
-  name       = "${var.project}-server-standy"
-  attach_eip = "true"
-  image_id   = "${var.image_id}"
-  flavor     = "${var.flavor}"
-  keypair    = "${var.keypair}"
-  network_id = "${var.network_id}"
-  subnet_id  = "${var.subnet_id}"
-  security_groups = ["${var.security_groups}"]
-  
-    allowed_address_pairs = [{"ip_address"="${openstack_networking_port_v2.port_vip.all_fixed_ips[0]}"}]
-  }]
-}
-
 resource "openstack_networking_port_v2" "port_vip" {
   name           = "${var.project}-port-vip"
   network_id     = "${var.network_id}"
   security_group_ids = ["${var.security_groups}"]
   admin_state_up = "true"
   allowed_address_pairs = [{"ip_address"="1.1.1.1/0"}]
+}
+
+resource "openstack_networking_port_v2" "port_master" {
+  depends_on  = ["openstack_networking_port_v2.port_vip"]
+  name           = "${openstack_networking_port_v2.port_vip.id}"
+  network_id     = "${var.network_id}"
+  security_group_ids = ["${var.security_groups}"]
+  admin_state_up = "true"
+  allowed_address_pairs = [{"ip_address"="${openstack_networking_port_v2.port_vip.all_fixed_ips[0]}"}]
+}
+
+resource "openstack_networking_port_v2" "port_standby" {
+  depends_on  = ["openstack_networking_port_v2.port_vip"]
+  name           = "${openstack_networking_port_v2.port_vip.id}"
+  network_id     = "${var.network_id}"
+  security_group_ids = ["${var.security_groups}"]
+  admin_state_up = "true"
+  allowed_address_pairs = [{"ip_address"="${openstack_networking_port_v2.port_vip.all_fixed_ips[0]}"}]
+}
+
+resource "openstack_compute_instance_v2" "instance_master" {
+  depends_on	= ["openstack_networking_port_v2.port_master"]
+  name = "${var.project}-server-master"
+  image_id = "${var.image_id}"
+  flavor_name = "${var.flavor}"
+  availability_zone = "${var.az}"
+  key_pair = "${var.keypair}"
+  security_groups = ["${var.security_groups}"]
+  network {
+	port = "${openstack_networking_port_v2.port_master.id}"
+  }
+}
+
+resource "openstack_compute_instance_v2" "instance_standby" {
+  depends_on  = ["openstack_networking_port_v2.port_standby"]
+  name = "${var.project}-server-standby"
+  image_id        = "${var.image_id}"
+  flavor_name          = "${var.flavor}"
+  availability_zone = "${var.az}"
+  key_pair = "${var.keypair}"
+  security_groups = ["${var.security_groups}"]
+  network {
+  port = "${openstack_networking_port_v2.port_standby.id}"
+  }
 }
--- stack(vip+snat)/outputs.tf
+++ stack(vip+snat)/outputs.tf
@@ -1,9 +1,14 @@
 output "addresses-master" {
   description = "list of ipv4 addresses of the created servers"
-  value       = ["${module.ecs-master.addresses}"]
+  value       = ["${openstack_compute_instance_v2.instance_master.*.access_ip_v4}"]
 }
 
 output "addresses-standby" {
   description = "list of ipv4 addresses of the created servers"
-  value       = ["${module.ecs-standby.addresses}"]
+  value       =  ["${openstack_compute_instance_v2.instance_standby.*.access_ip_v4}"]
+}
+
+output "addresses-vip" {
+  description = "list of ipv4 addresses of the created servers"
+  value       =  ["${openstack_networking_port_v2.port_vip.all_fixed_ips}"]
 }

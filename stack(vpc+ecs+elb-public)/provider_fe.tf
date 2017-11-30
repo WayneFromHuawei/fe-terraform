d299d2e2cf292a317cd354cfa5990b51d0e61639
WayneFromHuawei <wayne.wan@huawei.com>
2017-11-30 16:47:57 +0100

    elb template example
    this template example mixs the orangecloud ELB and openstack vpc and ecs to create a whole stack of public elastic load balancer and its backend servers

---
+++ stack(vpc+ecs+elb-public)/provider_fe.tf
@@ -0,0 +1,17 @@
+provider "orangecloud" {
+  user_name   = ""
+  tenant_name = "eu-west-0"
+  password    = ""
+  domain_id   = "" 
+  insecure = "true"
+  auth_url = "https://iam.eu-west-0.prod-cloud-ocb.orange-business.com/v3"
+}
+
+provider "openstack" {
+  user_name   = ""
+  tenant_name = "eu-west-0"
+  password    = ""
+  domain_id   = ""
+  insecure = "true"
+  auth_url = "https://iam.eu-west-0.prod-cloud-ocb.orange-business.com/v3"
+}
---
+++ stack(vpc+ecs+elb-public)/vars_example.tfvars
@@ -0,0 +1,9 @@
+region="eu-west-0"
+az_a="eu-west-0a"
+az_b="eu-west-0b"
+project="terrtest-public-elb"
+image_id="ab30dd89-ca93-4e0d-b23d-75fecdea8e4e"
+flavor="t2.micro"
+keypair="KeyPair-wanwei-ocb-1835"
+instance_count="2"
+security_groups=["fc5fb8b7-7c3d-4c41-9fc3-cf92d0e83ede"]
